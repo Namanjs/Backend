421733eb7ae0031f02df9faa68220a8467448b78
Naman <namanagarwal686@gmail.com>
2025-11-02 07:13:20 +0530

    added await

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -47,7 +47,7 @@ const userSchema = new Schema({
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next()
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
@@ -66,6 +66,7 @@ userSchema.methods.generateAccessToken = function () {
     }
     )
 }
+
 userSchema.methods.generateRefreshToken = function () {
     return jwt.sign({ // mostly instant, incase using a heavy algo consider using async await
         _id: this._id,
