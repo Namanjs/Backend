b67abf3d306a036f90d409aeb7ecf1511b10e5b4
Naman <namanagarwal686@gmail.com>
2025-11-09 18:41:44 +0530

    added the login and logout routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 // Create a new router instance
 const router = Router();
@@ -24,6 +25,11 @@ router.route("/register").post(
     // If upload is successful, move to controller
     registerUser
 );
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
 
 // Export router so it can be used in main application file
 export default router;
